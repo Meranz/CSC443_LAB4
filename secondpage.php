6da30a6c3362c97c851ef9f4f3c1dc7253ce685f
Meranz <2020813654@student.uitm.edu.my>
2022-06-08 01:39:40 +0800

    secondpage.php

--- secondpage.php
+++ secondpage.php
@@ -1,21 +1,16 @@
-<title>Document</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
 <?php include ("header.inc"); ?>
 
-<?php
-$$num = 22 % 2;
-if ($num = 2 || $num = 3 || $num = 4){
- 	echo "This is not the number"+" you're looking for";
-	}
-else if ($num = 5){
-echo "A snack is on its way to you!";
-}
-else
-echo "Ahaaaa Yes!";
-?>
 <br> <br>
 <a href="mainpage.php"> Return to Main Page </a>
 
 </body>
-</html> 
+</html>
