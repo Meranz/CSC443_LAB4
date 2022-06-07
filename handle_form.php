e459eb99fe11f8f4a5bcc80572608bd36e0b20c5
Meranz <2020813654@student.uitm.edu.my>
2022-06-08 01:00:12 +0800

    handle_form.php

---
+++ handle_form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="handle form.php" method="post"> <p><b>Name</b><input type="text" size="12" maxlength="12" name="name"><p/> <p><b>Email Address:</b> <input type="text" size="12" maxlength="36" name="email"><p/>
+
+<p><b>Gender</b>
+
+<input type="radio" name="gender" value="M" /> Male
+
+<input type="radio" name="gender" value="" /> Female </p>
+
+<p><b>Age:</b>
+
+<select name="age">
+
+
+<option value="0-30">Under 30</option> <option value="30-60">Between 30 and 60</option>
+
+<option value="60+">Over 60</option>
+
+</select></p>
+
+<p><b>Comments: </b> <textarea name="comments" rova""3" cols="50"> </textarea></p>
+
+<div align="center"> <input type="submit" name="submit" value="Submit Information" />
+
+</body> <html>
+</body>
+</html>
